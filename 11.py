568758fd60b22b4e6e7a36b537bc214efa9e98a8
daniel25204 <112447676+daniel25204@users.noreply.github.com>
2022-09-21 18:05:58 +0630

    Update 11.py

--- 11.py
+++ 11.py
@@ -57,3 +57,148 @@ print(fencing_value)
 
 
 
+###############################################################################################
+
+
+
+# 11.12.2.1
+Junior = {}
+credits = 0
+for c in Junior.values():
+    credits += c
+print(credits)
+
+
+
+
+# 11.12.2.2
+str1 = "peter piper picked a pack of pickeled peppers"
+freq = {}
+for char in str1:
+    if char not in freq.keys():
+        freq[char] = 1
+    else:
+        freq[char] = freq[char] + 1
+
+print(freq)
+
+
+
+
+
+# 11.12.2.3
+s1 = "hello"
+counts = {}
+for char in s1:
+    if char not in counts.keys():
+        counts[char] = 1
+    else:
+        counts[char] = counts[char] + 1
+
+print(counts)
+
+
+
+# 11.12.2.4
+str1 = "I wish I wish with all my heart to fly with dragons in a land apart"
+freq_words = {}
+words = str1.split(' ')
+for w in words:
+    if w not in freq_words.keys():
+        freq_words[w] = 1
+    else:
+        freq_words[w] = freq_words[w] + 1
+
+print(freq_words)
+
+
+
+
+# 11.12.2.5
+sent = "Singing in the rain and playing in the rain are two entirely different situations but both can be good"
+wrd_d = {}
+words = sent.split(' ')
+for w in words:
+    if w not in wrd_d.keys():
+        wrd_d[w] = 1
+    else:
+        wrd_d[w] = wrd_d[w] + 1
+
+print(wrd_d)
+
+
+
+
+# 11.12.2.6
+sally = "sally sells sea shells by the sea shore"
+characters = {}
+words = sally.split(' ')
+for w in words:
+    if w not in characters.keys():
+        characters[w] = 1
+    else:
+        characters[w] = characters[w] + 1
+
+best_char = ''
+largest_frequency = 0
+for char, freq in characters.items():
+    if freq > largest_frequency:
+        largest_frequency = freq
+        best_char = char
+    
+print(characters)
+print(best_char)
+
+
+
+
+
+# 11.12.2.7
+sally = "sally sells sea shells by the sea shore"
+characters = {}
+words = sally.split(' ')
+for w in words:
+    if w not in characters.keys():
+        characters[w] = 1
+    else:
+        characters[w] = characters[w] + 1
+
+worst_char = ''
+smallest_frequency = max(characters.values())
+for char, freq in characters.items():
+    if freq < smallest_frequency:
+        smallest_frequency = freq
+        worst_char = char
+    
+print(characters)
+print(worst_char)
+
+
+
+
+# 11.12.2.8
+string1 = "There is a tide in the affairs of men, Which taken at the flood, leads on to fortune. Omitted, all the voyage of their life is bound in shallows and in miseries. On such a full sea are we now afloat. And we must take the current when it serves, or lose our ventures."
+letter_counts = {}
+words = [word.lower() for word in string1.split(' ')]
+for w in words:
+    if w not in letter_counts.keys():
+        letter_counts[w] = 1
+    else:
+        letter_counts[w] = letter_counts[w] + 1
+
+print(letter_counts)
+
+
+
+
+# 11.12.2.9
+p = "Summer is a great time to go outside. You have to be careful of the sun though because of the heat."
+low_d = {}
+for char in p.lower():
+    if char not in low_d.keys():
+        low_d[char] = 1
+    else:
+        low_d[char] = low_d[char] + 1
+
+print(low_d)
+
