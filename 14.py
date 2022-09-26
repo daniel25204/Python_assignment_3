73685694715f390e446fedbcae544019ec40ca1d
daniel25204 <112447676+daniel25204@users.noreply.github.com>
2022-09-26 14:36:14 +0630

    2 err fixed

--- 14.py
+++ 14.py
@@ -1,27 +1,47 @@
 # 14.8.1
 def sublist(l):
-    ind = 0
-    num = l[0]
-    sl = []
-    while num != 5:
-        sl.append(l[ind])
-        ind += 1
-        num = l[ind]
-    return sl
+    if l != None:
+        if l == []:
+            return []
+        ind = 0
+        num = l[0]
+        sl = []
+        while num != 5:
+            sl.append(l[ind])
+            ind += 1
+            num = l[ind]
+        return sl
+    return None
+
+print(sublist([2,3,6,4,5,11,3]))
+print(sublist([5]))
+print(sublist([]))
+print(sublist([5, 4,24,1]))
+print(sublist(None))
 
 
 
 
 # 14.8.2
 def check_nums(l):
-    ind = 0
-    num = l[0]
-    sl = []
-    while num != 7:
-        sl.append(l[ind])
-        ind += 1
-        num = l[ind]
-    return sl
+    if l != None:
+        if l == []:
+            return []
+        ind = 0
+        num = l[0]
+        sl = []
+        while num != 7:
+            sl.append(l[ind])
+            ind += 1
+            num = l[ind]
+        return sl
+    return None
+
+print(check_nums([2,3,6,4,7,11,3]))
+print(check_nums([7]))
+print(check_nums([]))
+print(check_nums([7, 4,24,1]))
+print(check_nums(None))
 
 
 
